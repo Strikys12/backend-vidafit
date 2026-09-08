c0d0e79096cedb7fe107b07580cdff02b7e03d74
Strikys12 <daqui.99@hotmail.com>
2026-09-08 09:41:17 -0500

    Se inicia backend vidafit

---
+++ src/main/java/com/generation/vidafit/VidafitApplication.java
@@ -0,0 +1,13 @@
+package com.generation.vidafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidafitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/vidafit/VidafitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.vidafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
